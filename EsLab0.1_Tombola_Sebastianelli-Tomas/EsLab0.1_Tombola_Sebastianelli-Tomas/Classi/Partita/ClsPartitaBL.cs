55e583c36d4173eff2bb96373bd0f0931b78edff
Tomas Sebastianelli <79444403+TommySeb@users.noreply.github.com>
2023-11-20 10:30:16 +0100

    Inizio implementazione metodi partite
    Implementato metodo per ottenere tutte le partite (da testare) e iniziata l'implementazione del metodo per effettuare la ricerca di partite per nome.
    Inserito un file per il TODO dentro la soluzione.

--- EsLab0.1_Tombola_Sebastianelli-Tomas/EsLab0.1_Tombola_Sebastianelli-Tomas/Classi/Partita/ClsPartitaBL.cs
+++ EsLab0.1_Tombola_Sebastianelli-Tomas/EsLab0.1_Tombola_Sebastianelli-Tomas/Classi/Partita/ClsPartitaBL.cs
@@ -8,10 +8,59 @@ namespace EsLab0._1_Tombola_Sebastianelli_Tomas
 {
     class ClsPartitaBL
     {
-        #region Costruttore
-        ClsPartitaBL()
-        {
+        #region Variabili
+        DBManager _dbManager;
+        string _connectionString;
+        #endregion
 
+        #region Costruttore
+        ClsPartitaBL(DBManager dBManager)
+        {
+            _dbManager = dBManager;
+        }
+        #endregion
+
+        #region Metodi
+        public void OttieniDalDB()
+        {
+            // Dichiarazione variabili necessarie
+            System.Data.DataTable _partite;
+            string _errore = String.Empty;
+
+            // Query al DB
+            _partite = _dbManager.GetDataTableByQuery("SELECT * FROM partite", null, ref _errore);
+
+            // Creo i vari oggetti di classe partita se non si sono verificati errori
+            if (String.IsNullOrEmpty(_errore))
+            {
+                for(int i = 0; i < _partite.Rows.Count; i++)
+                {
+                    ClsPartitaDB _partita = new ClsPartitaDB();
+                    _partita.Id = (long)_partite.Rows[i]["id"];
+                    _partita.Prezzo = (Decimal)_partite.Rows[i]["prezzo"];
+                    _partita.ValoreAmbo = (Decimal)_partite.Rows[i]["valore_ambo"];
+                    _partita.ValoreTerna = (Decimal)_partite.Rows[i]["valore_terna"];
+                    _partita.ValoreQuaterna = (Decimal)_partite.Rows[i]["valore_quaterna"];
+                    _partita.ValoreCinquina = (Decimal)_partite.Rows[i]["valore_cinquina"];
+                    _partita.ValoreTombola = (Decimal)_partite.Rows[i]["valore_tombola"];
+
+                    Program._partite.Add(_partita);
+                }
+            }
+            else
+                throw new Exception(_errore);
+        }
+
+        public List<ClsPartitaDB> CercaPerNome(string nome)
+        {
+            // Risultato di ricerca
+            List<ClsPartitaDB> _partiteTrovate = new List<ClsPartitaDB>(0);
+
+            // Query al DB
+            _dbManager.GetDataTableByQuery("SELECT ID ")
+
+            // Ritorno il risultato di ricerca
+            return _partiteTrovate;
         }
         #endregion
     }
